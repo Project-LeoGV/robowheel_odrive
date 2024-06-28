10360bebf6872b0a96fb24f5d89a9b1c97f4bb5f
Marawan-Ahmed <marawanahrabee@gmail.com>
2024-06-28 05:44:21 +0300

    added feedbacks

--- odrive_driver/odrive_driver/odrive_node.py
+++ odrive_driver/odrive_driver/odrive_node.py
@@ -32,36 +32,39 @@ class ODriveNode(Node):
 
         # self.declare_parameter('connection.timeout', 15, ParameterDescriptor(
         #     type=ParameterType.PARAMETER_INTEGER, description='ODrive connection timeout in seconds'))
-        # self.declare_parameter('battery.max_voltage', 4.2 * 6, ParameterDescriptor(
-        #     type=ParameterType.PARAMETER_DOUBLE, description='Max battery voltage'))
-        # self.declare_parameter('battery.min_voltage', 3.2 * 6, ParameterDescriptor(
-        #     type=ParameterType.PARAMETER_DOUBLE, description='Min battery voltage'))
+        self.declare_parameter('battery.max_voltage', 4.2 * 10, ParameterDescriptor(
+            type=ParameterType.PARAMETER_DOUBLE, description='Max battery voltage'))
+        self.declare_parameter('battery.min_voltage', 3.2 * 10, ParameterDescriptor(
+            type=ParameterType.PARAMETER_DOUBLE, description='Min battery voltage'))
         # self.declare_parameter('battery.topic', 'barrery_percentage', ParameterDescriptor(
         #     type=ParameterType.PARAMETER_STRING, description='Battery percentage publisher topic'))
         # self.declare_parameter('joint_state.topic', 'joint_state', ParameterDescriptor(
         #     type=ParameterType.PARAMETER_STRING, description='Joint State publisher topic'))
         
-        self.axis0_vel_pub = self.create_publisher(
-            Float32, 'axis0_vel_pub', 50)
-        self.axis1_vel_pub = self.create_publisher(
-            Float32, 'axis1_vel_pub', 50)
 
-        self.axis0_pos_pub = self.create_publisher(
-            Float32, 'axis0_pos_pub', 50)
-        self.axis1_pos_pub = self.create_publisher(
-            Float32, 'axis1_pos_pub', 50)
-        
-        # Publishers
-        self.odom_publisher = self.create_publisher(Odometry, 'odom/unfiltered', 10)
-        # self.joint_state_publisher = self.create_publisher(JointState, 'joint_states', 10)
-        
         # Subscribers
         self.cmd_vel_subscriber = self.create_subscription(Twist, 'cmd_vel', self.cmd_vel_callback, 10)
         # #  Timers for publishing odometry and joint states
 
+        # Publishers
+        self.odom_publisher = self.create_publisher(Odometry, 'odom/unfiltered', 10)
+        self.joint_state_publisher = self.create_publisher(JointState, 'joint_states', 10)
+        self.battery_percentage_publisher = self.create_publisher(Float32, '/battery/precentage', 10)
+        self.battery_voltage_publisher = self.create_publisher(Float32, '/battery/voltage', 10)
+        self.battery_current_publisher = self.create_publisher(Float32, '/battery/current', 10)
+        self.battery_power_publisher = self.create_publisher(Float32, '/battery/power', 10)
+
+        self.left_motor_rpm_publisher = self.create_publisher(Float32, '/left_motor/rpm', 10)
+        self.left_motor_torque_publisher= self.create_publisher(Float32, '/left_motor/effort', 10)
+        self.right_motor_rpm_publisher = self.create_publisher(Float32, '/right_motor/rpm', 10)
+        self.right_motor_torque_publisher = self.create_publisher(Float32, '/right_motor/effort', 10)
+
         self.odom_timer = self.create_timer(0.1, self.publish_odometry)  # 10 Hz
         # self.joint_state_timer = self.create_timer(0.1, self.publish_joint_states)  # 10 Hz
-       
+        self.readings_feadback = self.create_timer(0.1, self.publish_feedback)  # 10 Hz
+
+
+
         # # Variables to store the state
         self.current_velocity = Twist()
         self.current_time = self.get_clock().now()
@@ -70,8 +73,7 @@ class ODriveNode(Node):
         self.x = 0.0
         self.y = 0.0
         self.theta = 0.0
-        # self.current_joint_state = JointState()
-        # self.current_odometry = Odometry()
+
         # self.connect_odrive_service = self.create_service(
         #     Trigger,
         #     'connect_odrive',
@@ -84,40 +86,6 @@ class ODriveNode(Node):
         #     self.request_state_callback
         # )
 
-        # self.battery_percentage_publisher_ = self.create_publisher(
-        #     Float32,
-        #     self.get_parameter(
-        #         'battery.topic').get_parameter_value().string_value,
-        #     1
-        # )
-        # self.battery_percentage_publisher_timer = self.create_timer(
-        #     10,
-        #     self.battery_percentage_publisher_callback
-        # )
-        # self.joint_state_publisher_ = self.create_publisher(
-        #     JointState,
-        #     self.get_parameter(
-        #         'joint_state.topic').get_parameter_value().string_value,
-        #     10
-        # )
-        # self.joint_state_publisher_timer = self.create_timer(
-        #     0.1,
-        #     self.joint_state_publisher_callback
-        # )
-
-        timer_period = 1 / 100
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-
-        # self.axis0_vel_sub = self.create_subscription(
-        #     Float32, 'axis0_vel_sub', self.axis0_vel_callback, 50)
-        # self.axis1_vel_sub = self.create_subscription(
-        #     Float32, 'axis1_vel_sub', self.axis1_vel_callback, 50)
-
-        # self.axis0_pos_sub = self.create_subscription(
-        #     Float32, 'axis0_pos_sub', self.axis0_pos_callback, 50)
-        # self.axis1_pos_sub = self.create_subscription(
-        #     Float32, 'axis1_pos_sub', self.axis1_pos_callback, 50)
-        
     # def is_driver_ready(self):
     #     if self.driver:
     #         try:
@@ -169,36 +137,28 @@ class ODriveNode(Node):
         
     #     return response
 
+    # def connect_odrive_callback(self, request: Trigger.Request, response: Trigger.Response):
+    #     try:
+    #         self.get_logger().info('Connecting to ODrive')
+    #         self.driver = odrive.find_any(
+    #             timeout=self.get_parameter(
+    #                 'connection.timeout'
+    #             ).get_parameter_value().integer_value)
+    #         self.get_logger().info('ODrive connected')
+    #         response.success = True
+    #         response.message = f'Connected to {self.driver.serial_number}'
+    #         if not self.driver.user_config_loaded:
+    #             self.get_logger().warn('ODrive user config not loaded')
+    #         self.driver.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+    #         self.driver.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+    #     except TimeoutError:
+    #         response.success = False
+    #         response.message = 'Timeout'
+    #     except:
+    #         response.success = False
+    #         response.message = 'Unexpected error:', sys.exc_info()[0]
 
-    def timer_callback(self):
-        msg = Float32()
-        msg.data = self.odrv0.left_vel_estimate_tps()*2*math.pi
-        self.axis0_vel_pub.publish(msg)
-
-        msg.data = self.odrv0.right_vel_estimate_tps()*2*math.pi
-        self.axis1_vel_pub.publish(msg)
-
-        msg.data = self.odrv0.left_pos_t()*2*math.pi
-        self.axis0_pos_pub.publish(msg)
-
-        msg.data = self.odrv0.right_pos_t()*2*math.pi
-        self.axis1_pos_pub.publish(msg)
-
-    # def axis0_vel_callback(self, msg):
-    #     print(f'axis0 vel: {msg}')
-    #     self.odrv0.command_velocity(0, msg.data)
-
-    # def axis1_vel_callback(self, msg):
-    #     self.get_logger().info(f'axis1 vel: {msg}')
-    #     self.odrv0.command_velocity(1, msg.data)
-
-    # def axis0_pos_callback(self, msg):
-    #     self.get_logger().info(f'axis0 pos: {msg}')
-    #     self.odrv0.command_position(0, msg.data)
-
-    # def axis1_pos_callback(self, msg):
-    #     self.get_logger().info(f'axis1 pos: {msg}')
-    #     self.odrv0.command_velocity(1, msg.data)
+    #     return response
 
     def cmd_vel_callback(self, msg: Twist):
         self.get_logger().info(f'Received velocity command: {msg}')
@@ -242,7 +202,6 @@ class ODriveNode(Node):
         self.y += delta_s * math.sin(self.theta)
         self.theta += delta_theta
   
-
         odom_msg = Odometry()
         odom_msg.header = Header()
         odom_msg.header.stamp = self.current_time.to_msg()
@@ -275,69 +234,65 @@ class ODriveNode(Node):
 
 
 
+    def publish_joint_states(self):
+        # Create and publish JointState message
+        joint_state_msg = JointState()
+        joint_state_msg.header = Header()
+        joint_state_msg.header.stamp = self.get_clock().now().to_msg()
+        joint_state_msg.name = ['left_wheel_joint', 'right_wheel_joint']
+        joint_state_msg.position = [self.odrv0.left_pos_rad(), self.odrv0.left_pos_rad()]
+        joint_state_msg.velocity = [self.odrv0.left_vel_estimate_radps(), self.odrv0.right_vel_estimate_radps()]
+
+        self.joint_state_publisher.publish(joint_state_msg)
 
 
-    # def publish_joint_states(self):
-    #     # Create and publish JointState message
-    #     joint_state_msg = JointState()
-    #     joint_state_msg.header = Header()
-    #     joint_state_msg.header.stamp = self.get_clock().now().to_msg()
-    #     joint_state_msg.name = ['left_wheel_joint', 'right_wheel_joint']
-    #     joint_state_msg.position = [self.odrv0.left_pos(), self.odrv0.right_pos()]
 
-    #     self.joint_state_publisher.publish(joint_state_msg)
+    def publish_feedback(self):
+        # if self.is_driver_ready():
+            msg = Float32()
+            msg.data = self.odrv0.vbus_voltage
+            self.battery_voltage_publisher.publish(msg)
 
-    # def connect_odrive_callback(self, request: Trigger.Request, response: Trigger.Response):
-    #     try:
-    #         self.get_logger().info('Connecting to ODrive')
-    #         self.driver = odrive.find_any(
-    #             timeout=self.get_parameter(
-    #                 'connection.timeout'
-    #             ).get_parameter_value().integer_value)
-    #         self.get_logger().info('ODrive connected')
-    #         response.success = True
-    #         response.message = f'Connected to {self.driver.serial_number}'
-    #         if not self.driver.user_config_loaded:
-    #             self.get_logger().warn('ODrive user config not loaded')
-    #         self.driver.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-    #         self.driver.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-    #     except TimeoutError:
-    #         response.success = False
-    #         response.message = 'Timeout'
-    #     except:
-    #         response.success = False
-    #         response.message = 'Unexpected error:', sys.exc_info()[0]
+            msg = Float32()
+            msg.data = (
+                self.odrv0.odrive.vbus_voltage -
+                self.get_parameter(
+                    'battery.min_voltage').get_parameter_value().double_value
+            ) / (
+                self.get_parameter('battery.max_voltage').get_parameter_value().double_value -
+                self.get_parameter('battery.min_voltage').get_parameter_value().double_value)
+            self.battery_percentage_publisher.publish(msg)
 
-    #     return response
+            if msg.data < 0.2:
+                self.get_logger().warn(
+                    f'ODrive battery percentage low: {msg.data:0.2f}')
+                
+            msg = Float32()
+            msg.data = self.odrv0.odrive.ibus
+            self.battery_current_publisher.publish(msg)   
 
-    # def battery_percentage_publisher_callback(self):
-    #     if self.is_driver_ready():
-    #         msg = Float32()
-    #         msg.data = (
-    #             self.driver.vbus_voltage -
-    #             self.get_parameter(
-    #                 'battery.min_voltage').get_parameter_value().double_value
-    #         ) / (
-    #             self.get_parameter('battery.max_voltage').get_parameter_value().double_value -
-    #             self.get_parameter('battery.min_voltage').get_parameter_value().double_value)
-    #         self.battery_percentage_publisher_.publish(msg)
-    #         if msg.data < 0.2:
-    #             self.get_logger().warn(
-    #                 f'ODrive battery percentage low: {msg.data:0.2f}')
-    #     else:
-    #         self.get_logger().debug('ODrive not ready')
+            msg = Float32()
+            msg.data = self.odrv0.odrive.ibus * self.odrv0.odrive.vbus_voltage
+            self.battery_power_publisher.publish(msg) 
 
-    # def joint_state_publisher_callback(self):
-    #     if self.is_driver_ready():
-    #         msg = JointState()
-    #         msg.header.stamp = self.get_clock().now().to_msg()
-    #         msg.position = [self.driver.axis0.encoder.pos_estimate,
-    #                         self.driver.axis1.encoder.pos_estimate]
-    #         msg.velocity = [self.driver.axis0.encoder.vel_estimate,
-    #                         self.driver.axis1.encoder.vel_estimate]
-    #         self.joint_state_publisher_.publish(msg)
-    #     else:
-    #         self.get_logger().debug('ODrive not ready')
+            msg = Float32()
+            msg.data = self.odrv0.left_vel_estimate_tps() * 60 
+            self.left_motor_rpm_publisher.publish(msg)  
+
+            msg = Float32()
+            msg.data = self.odrv0.left_axis.motor.torque_estimate
+            self.left_motor_torque_publisher.publish(msg) 
+
+            msg = Float32()
+            msg.data = self.odrv0.right_vel_estimate_tps() * 60 
+            self.right_motor_rpm_publisher.publish(msg)    
+
+            msg = Float32()
+            msg.data = self.odrv0.right_axis.motor.torque_estimate
+            self.right_motor_torque_publisher.publish(msg)    
+        # else:
+        #     self.get_logger().debug('ODrive not ready')
+
 
 
 
